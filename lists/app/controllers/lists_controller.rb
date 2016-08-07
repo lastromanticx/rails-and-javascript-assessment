136ced805a60132cfaa8cbee1ea1b1362e5afe10
lastromanticx <giladbarkan@live.com>
2016-08-07 14:47:22 -0400

    fixed return redirect to avoid multiple redirects

--- lists/app/controllers/lists_controller.rb
+++ lists/app/controllers/lists_controller.rb
@@ -12,7 +12,7 @@ class ListsController < ApplicationController
     if @list.nil?
       not_found
     elsif !authorize_resource(current_user,@list,:show)
-      redirect_to user_path(current_user)
+      return redirect_to lists_path
     end
     @task = Task.new
     
@@ -43,7 +43,7 @@ class ListsController < ApplicationController
     if @list.nil?
       not_found
     elsif !authorize_resource(current_user,@list,:edit)
-      redirect_to lists_path
+      return redirect_to lists_path
     end
   end
 
