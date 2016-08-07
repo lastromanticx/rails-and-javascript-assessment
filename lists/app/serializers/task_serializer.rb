f6082c36323c8123e9d514578e6af071d304820e
lastromanticx <giladbarkan@live.com>
2016-08-07 19:50:25 -0400

    added test for serializing task.format_due_date method to avoid error

--- lists/app/serializers/task_serializer.rb
+++ lists/app/serializers/task_serializer.rb
@@ -1,4 +1,8 @@
 class TaskSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :format_due_date, :status, :errors
   has_many :tags
+
+  def format_due_date
+    object.format_due_date if object.due_date
+  end
 end
