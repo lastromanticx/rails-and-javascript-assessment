04c54f8a656861b85bd01aefd331580e6e86741f
lastromanticx <giladbarkan@live.com>
2016-08-07 14:19:16 -0400

    working on lists show page with jqery

--- lists/app/serializers/list_serializer.rb
+++ lists/app/serializers/list_serializer.rb
@@ -4,6 +4,10 @@ class ListSerializer < ActiveModel::Serializer
   attributes :id, :name, :collaborators, :editable
   has_many :tasks, serializer: ListTaskSerializer
 
+  def collaborators
+    object.collaborators(true)
+  end
+
   def editable
     authorize_resource(current_user,object,:edit)
   end
