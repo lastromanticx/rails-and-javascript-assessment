86ee1a66dff95895feb66ee677c39ec16d7ae602
lastromanticx <giladbarkan@live.com>
2016-08-07 21:16:56 -0400

    added javascript functionality for adding new tag without refresh

--- lists/app/assets/javascripts/models.js
+++ lists/app/assets/javascripts/models.js
@@ -26,10 +26,18 @@ var Task = function(task_hash){
   this.status = task_hash.status;
   this.format_due_date = task_hash.format_due_date;
   this.listId = task_hash.list_id;
+  this.tags = task_hash.tags
   this.errors = task_hash.errors;
 
   this.linkHTMLString = function(){
     return '<a href="/lists/' + this.listId + '/tasks/' + this.id + '">' + this.name + '</a>'
+  }
+
+  this.listTaskHTML = function(){
+    return '<h5>' + this.linkHTMLString() + '</h5>' +
+           '<p>Due: ' + this.format_due_date + '<p>' +
+           '<p>Status:  ' + this.status + '</p>' +
+           '<hr >'
   }
 
   this.format_errors = function(){
